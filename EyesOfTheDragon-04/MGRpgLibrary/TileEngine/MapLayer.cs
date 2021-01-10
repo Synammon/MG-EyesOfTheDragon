80651744ec694a8f106a32cc24b5ac00057c9c72
Cynthia McMahohn <cynth.mcmahon@gmail.com>
2021-01-10 12:54:51 -0500

    Tutorial 04 committed

---
+++ EyesOfTheDragon-04/MGRpgLibrary/TileEngine/MapLayer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MGRpgLibrary.TileEngine
+{
+    public class MapLayer
+    {
+        #region Field Region
+
+        Tile[,] map;
+
+        #endregion
+
+        #region Property Region
+
+        public int Width
+        {
+            get { return map.GetLength(1); }
+        }
+
+        public int Height
+        {
+            get { return map.GetLength(0); }
+        }
+
+        #endregion
+
+        #region Constructor Region
+
+        public MapLayer(Tile[,] map)
+        {
+            this.map = (Tile[,])map.Clone();
+        }
+
+        public MapLayer(int width, int height)
+        {
+            map = new Tile[height, width];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    map[y, x] = new Tile(0, 0);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Method Region
+
+        public Tile GetTile(int x, int y)
+        {
+            return map[y, x];
+        }
+
+        public void SetTile(int x, int y, Tile tile)
+        {
+            map[y, x] = tile;
+        }
+
+        public void SetTile(int x, int y, int tileIndex, int tileset)
+        {
+            map[y, x] = new Tile(tileIndex, tileset);
+        }
+
+        #endregion
+    }
+}
---
+++ EyesOfTheDragon/MGRpgLibrary/TileEngine/TileMap.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MGRpgLibrary.TileEngine
+{
+    public class TileMap
+    {
+        #region Field Region
+        
+        List<Tileset> tilesets;
+        List<MapLayer> mapLayers;
+        
+        #endregion
+        
+        #region Property Region
+        #endregion
+        
+        #region Constructor Region
+        
+        public TileMap(List<Tileset> tilesets, List<MapLayer> layers)
+        {
+            this.tilesets = tilesets;
+            this.mapLayers = layers;
+        }
+        public TileMap(Tileset tileset, MapLayer layer)
+        {
+            tilesets = new List<Tileset>();
+            tilesets.Add(tileset);
+            mapLayers = new List<MapLayer>();
+            mapLayers.Add(layer);
+        }
+        
+        #endregion
+        
+        #region Method Region
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle destination = new Rectangle(0, 0, Engine.TileWidth, Engine.TileHeight);
+            Tile tile;
+
+            foreach (MapLayer layer in mapLayers)
+            {
+                for (int y = 0; y < layer.Height; y++)
+                {
+                    destination.Y = y * Engine.TileHeight;
+                    
+                    for (int x = 0; x < layer.Width; x++)
+                    {
+                        tile = layer.GetTile(x, y);
+                        
+                        destination.X = x * Engine.TileWidth;
+            
+                        spriteBatch.Draw(
+                            tilesets[tile.Tileset].Texture,
+                            destination,
+                            tilesets[tile.Tileset].SourceRectangles[tile.TileIndex],
+                        Color.White);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
