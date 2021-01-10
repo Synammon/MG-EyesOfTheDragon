5a61f7089d7c7a128c2c4eec93892e22a43337dc
Cynthia McMahohn <cynth.mcmahon@gmail.com>
2021-01-09 22:43:12 -0500

    Tutorial 03

---
+++ EyesOfTheDragon/EyesOfTheDragon/GameScreens/GamePlayScreen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MGRpgLibrary;
+
+namespace EyesOfTheDragon.GameScreens
+{
+    public class GamePlayScreen : BaseGameState
+    {
+        #region Field Region
+        #endregion
+        
+        #region Property Region
+        #endregion
+        
+        #region Constructor Region
+        
+        public GamePlayScreen(Game game, GameStateManager manager)
+            : base(game, manager)
+        {
+        }
+
+        #endregion
+
+        #region XNA Method Region
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        #endregion
+
+        #region Abstract Method Region
+        #endregion
+    }
+}
