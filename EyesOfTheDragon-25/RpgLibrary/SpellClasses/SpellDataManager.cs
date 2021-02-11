b311ca909939c69d428a775c74f1fb7f87f00a63
Cynthia McMahohn <cynth.mcmahon@gmail.com>
2021-02-11 09:20:31 -0500

    Tutorial 25 Commit

---
+++ EyesOfTheDragon-25/RpgLibrary/SpellClasses/SpellDataManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpgLibrary.SpellClasses
+{
+    public class SpellDataManager
+    {
+        #region Field Region
+
+        readonly Dictionary<string, SpellData> spellData;
+
+        #endregion
+
+        #region Property Region
+
+        public Dictionary<string, SpellData> SpellData
+        {
+            get { return spellData; }
+        }
+
+        #endregion
+
+        #region Constructor Region
+
+        public SpellDataManager()
+        {
+            spellData = new Dictionary<string, SpellData>();
+        }
+
+        #endregion
+
+        #region Method Region
+        #endregion
+
+        #region Virtual Method region
+        #endregion
+    }
+}
