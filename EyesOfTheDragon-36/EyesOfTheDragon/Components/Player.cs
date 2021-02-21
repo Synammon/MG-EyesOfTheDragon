014ce9689d270f2e602f72ac260d289afe345b3c
Cynthia McMahohn <cynth.mcmahon@gmail.com>
2021-02-21 16:40:46 -0500

    Committing Tutorial 35 and 36

---
+++ EyesOfTheDragon-36/EyesOfTheDragon/Components/Player.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MGRpgLibrary;
+using MGRpgLibrary.TileEngine;
+using MGRpgLibrary.SpriteClasses;
+using MGRpgLibrary.CharacterClasses;
+using EyesOfTheDragon.GameScreens;
+
+namespace EyesOfTheDragon.Components
+{
+    public class Player
+    {
+        #region Field Region
+
+        Camera camera;
+        Game1 gameRef;
+        readonly Character character;
+
+        #endregion
+
+        #region Property Region
+
+        public Camera Camera
+        {
+            get { return camera; }
+            set { camera = value; }
+        }
+
+        public AnimatedSprite Sprite
+        {
+            get { return character.Sprite; }
+        }
+
+        public Character Character
+        {
+            get { return character; }
+        }
+
+        #endregion
+
+        #region Constructor Region
+
+        public Player(Game game, Character character)
+        {
+            gameRef = (Game1)game;
+            camera = new Camera(gameRef.ScreenRectangle);
+            this.character = character;
+        }
+
+        #endregion
+
+        #region Method Region
+
+        public void Update(GameTime gameTime)
+        {
+            
+            camera.Update(gameTime);
+            Sprite.Update(gameTime);
+
+            if (InputHandler.KeyReleased(Keys.PageUp) ||
+                InputHandler.ButtonReleased(Buttons.LeftShoulder, PlayerIndex.One))
+            {
+                camera.ZoomIn();
+                
+                if (camera.CameraMode == CameraMode.Follow)
+                {
+                    camera.LockToSprite(Sprite);
+                }
+            }
+            else if (InputHandler.KeyReleased(Keys.PageDown) ||
+                InputHandler.ButtonReleased(Buttons.RightShoulder, PlayerIndex.One))
+            {
+                camera.ZoomOut();
+            
+                if (camera.CameraMode == CameraMode.Follow)
+                {
+                    camera.LockToSprite(Sprite);
+                }
+            }
+
+            Vector2 motion = new Vector2();
+            
+            if (InputHandler.KeyDown(Keys.W) ||
+                InputHandler.ButtonDown(Buttons.LeftThumbstickUp, PlayerIndex.One))
+            {
+                Sprite.CurrentAnimation = AnimationKey.Up;
+                motion.Y = -1;
+            }
+            else if (InputHandler.KeyDown(Keys.S) ||
+                InputHandler.ButtonDown(Buttons.LeftThumbstickDown, PlayerIndex.One))
+            {
+                Sprite.CurrentAnimation = AnimationKey.Down;
+                motion.Y = 1;
+            }
+        
+            if (InputHandler.KeyDown(Keys.A) ||
+                InputHandler.ButtonDown(Buttons.LeftThumbstickLeft, PlayerIndex.One))
+            {
+                Sprite.CurrentAnimation = AnimationKey.Left;
+                motion.X = -1;
+            }
+            else if (InputHandler.KeyDown(Keys.D) ||
+                InputHandler.ButtonDown(Buttons.LeftThumbstickRight, PlayerIndex.One))
+            {
+                Sprite.CurrentAnimation = AnimationKey.Right;
+                motion.X = 1;
+            }
+
+            if (motion != Vector2.Zero)
+            {
+                UpdatePosition(gameTime, motion);
+            }
+            else
+            {
+                Sprite.IsAnimating = false;
+            }
+
+            if (InputHandler.KeyReleased(Keys.F) ||
+                InputHandler.ButtonReleased(Buttons.RightStick, PlayerIndex.One))
+            {
+                camera.ToggleCameraMode();
+            
+                if (camera.CameraMode == CameraMode.Follow)
+                {
+                    camera.LockToSprite(Sprite);
+                }
+            }
+    
+            if (camera.CameraMode != CameraMode.Follow)
+            {
+                if (InputHandler.KeyReleased(Keys.C) ||
+                    InputHandler.ButtonReleased(Buttons.LeftStick, PlayerIndex.One))
+                {
+                    camera.LockToSprite(Sprite);
+                }
+            }
+        }
+
+        private void UpdatePosition(GameTime gameTime, Vector2 motion)
+        {
+            Sprite.IsAnimating = true;
+            motion.Normalize();
+            
+            Vector2 distance = motion * Sprite.Speed *
+                (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Vector2 next = distance + Sprite.Position;
+    
+            Rectangle playerRect = new Rectangle(
+                (int)next.X + CollisionLayer.CollisionRadius,
+                (int)next.Y + CollisionLayer.CollisionRadius,
+                Engine.TileWidth - CollisionLayer.CollisionRadius,
+                Engine.TileHeight - CollisionLayer.CollisionRadius);
+            foreach (Point p in
+                GamePlayScreen.World.CurrentMap.CollisionLayer.Collisions.Keys)
+            {
+                Rectangle r = new Rectangle(
+                    p.X * Engine.TileWidth + CollisionLayer.CollisionRadius,
+                    p.Y * Engine.TileHeight + CollisionLayer.CollisionRadius,
+                    Engine.TileWidth - CollisionLayer.CollisionRadius,
+                    Engine.TileHeight - CollisionLayer.CollisionRadius);
+            
+                if (r.Intersects(playerRect))
+                {
+                    return;
+                }
+            }
+
+            foreach (Character c in
+                GamePlayScreen.World.Levels[GamePlayScreen.World.CurrentLevel].Characters)
+            {
+                Rectangle r = new Rectangle(
+                    (int)c.Sprite.Position.X + Character.CollisionRadius,
+                    (int)c.Sprite.Position.Y + Character.CollisionRadius,
+                    Engine.TileWidth - Character.CollisionRadius,
+                    Engine.TileHeight - Character.CollisionRadius);
+
+                if (r.Intersects(playerRect))
+                {
+                    return;
+                }
+            }
+
+            Sprite.Position = next;
+            Sprite.LockToMap();
+
+            if (camera.CameraMode == CameraMode.Follow)
+            {
+                camera.LockToSprite(Sprite);
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+
+        {
+            character.Draw(gameTime, spriteBatch);
+        }
+        #endregion
+    }
+}
---
+++ EyesOfTheDragon-36/MGRpgLibrary/ItemClasses/ItemManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpgLibrary.ItemClasses
+{
+    public class ItemManager
+    {
+        #region Fields Region
+
+        Dictionary<string, Weapon> weapons = new Dictionary<string, Weapon>();
+        Dictionary<string, Armor> armors = new Dictionary<string, Armor>();
+        Dictionary<string, Shield> shields = new Dictionary<string, Shield>();
+
+        #endregion
+
+        #region Keys Property Region
+
+        public Dictionary<string, Weapon>.KeyCollection WeaponKeys
+        {
+            get { return weapons.Keys; }
+        }
+
+        public Dictionary<string, Armor>.KeyCollection ArmorKeys
+        {
+            get { return armors.Keys; }
+        }
+
+        public Dictionary<string, Shield>.KeyCollection ShieldKeys
+        {
+            get { return shields.Keys; }
+        }
+
+        #endregion
+
+        #region Constructor Region
+        public ItemManager()
+        {
+        }
+
+        #endregion
+
+        #region Weapon Methods
+
+        public void AddWeapon(Weapon weapon)
+        {
+            if (!weapons.ContainsKey(weapon.Name))
+            {
+                weapons.Add(weapon.Name, weapon);
+            }
+        }
+
+        public Weapon GetWeapon(string name)
+        {
+            if (weapons.ContainsKey(name))
+            {
+                return (Weapon)weapons[name].Clone();
+            }
+
+            return null;
+        }
+
+        public bool ContainsWeapon(string name)
+        {
+            return weapons.ContainsKey(name);
+        }
+
+        #endregion
+
+        #region Armor Methods
+
+        public void AddArmor(Armor armor)
+        {
+            if (!armors.ContainsKey(armor.Name))
+            {
+                armors.Add(armor.Name, armor);
+            }
+        }
+
+        public Armor GetArmor(string name)
+        {
+            if (armors.ContainsKey(name))
+            {
+                return (Armor)armors[name].Clone();
+            }
+
+            return null;
+        }
+
+        public bool ContainsArmor(string name)
+        {
+            return armors.ContainsKey(name);
+        }
+
+        #endregion
+
+        #region Shield Methods
+
+        public void AddShield(Shield shield)
+        {
+            if (!shields.ContainsKey(shield.Name))
+            {
+                shields.Add(shield.Name, shield);
+            }
+        }
+
+        public Shield GetShield(string name)
+        {
+            if (shields.ContainsKey(name))
+            {
+                return (Shield)shields[name].Clone();
+            }
+
+            return null;
+        }
+
+        public bool ContainsShield(string name)
+        {
+            return shields.ContainsKey(name);
+        }
+
+        #endregion
+    }
+}
